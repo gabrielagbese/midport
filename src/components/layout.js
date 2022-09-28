4c5156f61103d5ab1cb072e12c7ca6f890a16f82
therealbeez <gabriel.agbese2@gmail.com>
2022-09-29 01:31:29 +0200

    basic tweening done other elements still funky

--- src/components/layout.js
+++ src/components/layout.js
@@ -29,24 +29,72 @@ const Layout = ({ children }) => {
   return (
     <div className="external">
       <div className="container">
-        
+        <div className="hero text" onClick={() => {heroEx()}}>HELLO, WORLD</div>
+        <div className="about" onClick={() => {aboutEx()}}></div>
+        <div className="other">
+          <div className="projects" onClick={() => {projectsEx()}}></div>
+          <div className="contact" onClick={() => {contactEx()}}></div>
+        </div>
       </div>
     </div>
   )
 }
 
-// var topRight = gsap.timeline();
-// var topLeft = gsap.timeline();
+//hero timelines
+var hero1 = gsap.timeline();
+var hero2 = gsap.timeline();
+var hero3 = gsap.timeline();
+//about timelines
+var about1 = gsap.timeline();
+var about2 = gsap.timeline();
+var about3 = gsap.timeline();
+//other timelines
+var other1 = gsap.timeline();
+var other2 = gsap.timeline();
+var other3 = gsap.timeline();
+var projects = gsap.timeline();
+var contact = gsap.timeline();
 
 
-// function heroAbout(){
-//   topRight.to(".top-right",{ x: 45+'vw',});
-//   console.log("rightMoved")
-//   topLeft.to(".top-left", { y: -35+'vh',});
-//   topLeft.to(".top-left", { x: -50+'vw',});
+
+function heroEx(){
+  hero3.to(".other", { width: 25+"%",})
+  hero1.to(".hero", { width: 40+"%",})
+  hero2.to(".about", { width: 30+"%",})
+  othersReset()
+}
+
+function aboutEx(){
+  about2.to(".other", { width: 25+"%",})
+  about1.to(".hero", { width: 15+"%",})
+  about1.to(".about", { width: 55+"%",})
+
   
-  
-// }
+}
+
+function otherEx(){
+  other1.to(".about", { width: 10+"%",})
+  other1.to(".hero", { width: 15+"%",})
+  other1.to(".other", { width: 70+"%",})
+}
+
+function projectsEx(){
+  otherEx()
+  projects.to(".projects", { height: 90+"%",})
+  contact.to(".contact", { height: 10+"%",})
+}
+
+function contactEx(){
+  otherEx()
+  contact.to(".contact", { height: 90+"%",})
+  projects.to(".projects", { height: 10+"%",})
+}
+
+function othersReset(){
+  projects.to(".projects", {height: 50+"%", width: 100+"%"})
+  contact.to(".contact", {height: 50+"%", width: 100+"%"})
+}
+
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
