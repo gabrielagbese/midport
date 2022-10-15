fdb071eda3d310a2c8f8f1fd97e6b6ebeda0f0fc
therealbeez <gabriel.agbese2@gmail.com>
2022-10-15 20:20:32 +0200

    font, other section, color change

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,15 @@
 import * as React from "react"
+import testImage from "../images/testface.png"
 
 const Hero = () => {
+
+    const Styled = {
+        paddingLeft: 40,
+    }
     return(
-        <div>
-            <p className="text">Gabriel</p>
+        <div className="xx">
+            {/* <img src={testImage} width="400px" height="350px" style={Styled}></img> */}
+            <p className="text">Gabriel.</p>
         </div>
     )
 }
